5793f2ece2126b16022a6277844a95da65ccabc8
Aditya Raman <32497523+ramanaditya@users.noreply.github.com>
2018-10-04 02:08:51 +0530

    Create sample.c

---
+++ C/sample.c
@@ -0,0 +1,11 @@
+// header files goes here
+#include<stdio.h>
+
+int main()
+{
+  // Declare the variables here, Use variables with relative names don't just write a,b,c use sum, prod, result instead
+  
+  // Write your program here
+  
+  return 0;
+}
