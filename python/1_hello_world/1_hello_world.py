6e355a490da1f8ccba9171b79044ccbb1aa079b2
Aditya Raman <32497523+ramanaditya@users.noreply.github.com>
2018-10-04 19:08:27 +0530

    Update 1_hello_world.py

--- python/1_hello_world/1_hello_world.py
+++ python/1_hello_world/1_hello_world.py
@@ -7,7 +7,8 @@
 #----------------------------------------------------------
 '''
 We have already written hello world program for you need to run
-this in your terminal. Try to run it and then in place of 
+this in your terminal. Try to run it and then create a new file 
+with filename as your naem or USN and in that file in place of 
 'Hello' write your first_name and in place of 'world' write
 your last_name, run the program then make a pull request in our
 repository
