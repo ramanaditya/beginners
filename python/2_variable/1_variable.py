3ec2928ce80ae6e7102c3b23a91bd56bc4b12437
ramanaditya <adityaraman96@gmail.com>
2018-10-04 18:07:19 +0530

    question updated

---
+++ python/2_variable/1_variable.py
@@ -0,0 +1,15 @@
+#----------------------------------------------------------
+# sample.py
+# Uses python3
+# Created by: Aditya Raman
+#
+# A simple program that prints 'Hello World!'
+#----------------------------------------------------------
+'''
+
+'''
+#----------------------------------------------------------
+
+first_name = 'Hello'
+last_name = 'World!'
+print(first_name + ' ' + last_name)
