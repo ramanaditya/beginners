9438c4f078068e38015608d6e181574324f5e754
ramanaditya <adityaraman96@gmail.com>
2018-10-04 11:46:50 +0530

    initial commit

---
+++ python/README.md
@@ -0,0 +1,1 @@
+# General way of writing python program
---
+++ python/sample.py
@@ -0,0 +1,19 @@
+#----------------------------------------------------------
+# sample.py
+# Uses python3
+# Created by: Aditya Raman
+#
+# A simple program that prints 'Hello World!'
+#----------------------------------------------------------
+
+# All import statements should go here
+
+
+# def your function
+
+
+# main program starts here
+
+first_name = 'Hello'
+last_name = 'World!'
+print(first_name + ' ' + last_name)
