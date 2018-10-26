121c7b44fcf0909da0f3126722ae3792a6d782c4
anirudh-kac <anirudh.kac@yahoo.com>
2018-10-26 23:16:00 +0530

    solved the problem

--- C/2_Variable/2_variable.c
+++ C/2_Variable/2_variable.c
@@ -7,7 +7,17 @@ and print the total marks obtained and percentage as output
 
 int main()
 {
-  //Your code goes here
+// saving marks in physics ,chemistry and maths in variables phy,chem and math respectively
+// marks are out of 100 in each subject
+	float phy = 50;
+	float chem = 35;
+	float math= 99;
+	float total;
+	total = phy +chem +math;
+	float perc;
+	perc = total/300*100;
+	printf("your total marks are %.1f and percentage is %.2f ",total,perc);
+
   
   return 0;
 }
