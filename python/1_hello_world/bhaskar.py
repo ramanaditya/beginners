8cbc0303db9d9e171b7ab4c860307dc6287b35a5
bhashkarjishu <45569062+bhashkarjishu@users.noreply.github.com>
2019-03-15 19:47:56 +0530

    Create bhaskar.py

---
+++ python/1_hello_world/bhaskar.py
@@ -0,0 +1,20 @@
+#----------------------------------------------------------
+# sample.py
+# Uses python3
+# Created by: Aditya Raman
+#
+# A simple program that prints 'Hello World!'
+#----------------------------------------------------------
+'''
+We have already written hello world program for you need to run
+this in your terminal. Try to run it and then create a new file 
+with filename as your naem or USN and in that file in place of 
+'Hello' write your first_name and in place of 'world' write
+your last_name, run the program then make a pull request in our
+repository
+'''
+#----------------------------------------------------------
+	
+first_name = 'Bhaskar'
+last_name = 'Chakrabarty'
+print(first_name + ' ' + last_name)
