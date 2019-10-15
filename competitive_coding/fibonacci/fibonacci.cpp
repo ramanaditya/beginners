058cda0b6c0f1c7ef7060e42d5317a7cb759f5b6
Abhishek jain <abhijain03102000@gmail.com>
2019-10-15 11:55:32 +0530

    Hacktoberfest Abhishek jain

--- competitive_coding/fibonacci/fibonacci.cpp
+++ competitive_coding/fibonacci/fibonacci.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
+
 using namespace std;
+
 int main()
 {
 	int n;
-	cout<<"Enter the number of terms to find \n";
-	cin>>n;
-	int a=0,b=1;
-	cout<<a<<" "<<b<<" ";
+	cout << "Enter the number of terms to find \n";
+	cin >> n;
+	int a=0, b=1;
+	cout << a << " " << b << " ";
 	int c;
-	for(int i=0;i<n;i++)
+	for(int i=0; i<n; i++)
 	{
 		int c=a+b;
-		cout<<c<<" ";
+		cout << c << " ";
 		a=b;
 		b=c;
 	}
