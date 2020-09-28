5b34562efa9defde7937c4dc823c6d766209a61a
Atharva Kadlag <atharvakadlag@gmail.com>
2020-09-28 11:10:02 +0530

    Update Armstrong.py
    Added function to get digits, added few python keywords so that readers can lear pythonic ways to do things

--- python/Armstrong.py
+++ python/Armstrong.py
@@ -1,13 +1,23 @@
+# In number theory, a narcissistic number in a given number base 
+# is a number that is the sum of its own digits each raised to the power of the number of digits.
+
+# Armstrong number is any 3 digit narcissitstic number
+
+def get_digits(num):
+    list_of_digits = []
+    while num > 0:
+        digit = num % 10
+        list_of_digits.append(digit)
+        num //= 10
+    return list_of_digits
+
 num = int(input("Enter a number: "))  
-sum = 0  
-temp = num  
-  
-while temp > 0:  
-   digit = temp % 10  
-   sum += digit ** 3  
-   temp //= 10  
-  
-if num == sum:  
+
+list_of_digits = get_digits(num)
+
+list_of_cubes = [i**3 for i in list_of_digits]
+
+if num == sum(list_of_cubes):  
    print(num,"is an Armstrong number")  
 else:  
    print(num,"is not an Armstrong number")  
